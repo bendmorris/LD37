70ad32e8c5ac53d1c0f48484f2d86460dff5351d
Ben Morris <ben@bendmorris.com>
2016-12-17 22:08:03 -0800

    Fade out of title screen.

--- src/whathaveidone/scenes/TitleScene.hx
+++ src/whathaveidone/scenes/TitleScene.hx
@@ -9,24 +9,40 @@ import haxepunk.utils.Input;
 
 class TitleScene extends Scene
 {
+	static inline var FADE_TIME:Float = 0.75;
+
+	var fade:Bool = false;
+	var txt:BitmapText;
+
 	public function new()
 	{
 		super();
 
-		var txt = new BitmapText(
+		txt = new BitmapText(
 			"WHAT HAVE I DONE?\n\nA virtual pet simulator\n\nCreated by bendmorris for Ludum Dare 37\n\nClick to begin",
 			{font: 'assets/fonts/a_little_sunshine_regular_48.fnt'}
 		);
 		txt.x = 16;
 		txt.y = 16;
 		addGraphic(txt);
+
+		color = 0;
+		alpha = 1;
 	}
 
 	override public function update()
 	{
-		if (Input.mousePressed)
+		if (fade)
 		{
-			HXP.engine.popScene();
+			txt.alpha = (alpha -= HXP.elapsed / FADE_TIME);
+			if (alpha <= 0)
+			{
+				HXP.engine.popScene();
+			}
+		}
+		else if (Input.mousePressed)
+		{
+			fade = true;
 		}
 	}
 }
